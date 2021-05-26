5e9f7a27b6770b763da0295f5fc54b99f69fb56c
dpiyumantha <dimalkapiyumantha@gmail.com>
2021-05-26 16:17:40 +0530

    feat: missing number on ends of the sequence

--- missing-number/missing-number.js
+++ missing-number/missing-number.js
@@ -5,8 +5,17 @@ function findMissingNumber(arr){
             return arr[i]+1;
         }
     }
-    return null;
+    return [(arr[0]-1),(arr[arr.length-1]+1)];
 }
-console.log(
-findMissingNumber([1,3,4,6,7,2,5,10,9])
-)
+
+function printMissingNumber(arr){
+    console.log("Given sequence : ",arr)
+    console.log("You're missing, ");
+    Array.isArray(res=findMissingNumber(arr))?console.log(`${res[0]} or ${res[1]}`)
+    :console.log(res)
+}
+
+
+printMissingNumber( [ 1, 3, 4, 6, 7, 2, 5, 10, 9])
+printMissingNumber( [ 1, 3, 8, 4, 6, 7, 2, 5, 10, 9])
+
