ab12a7875ad1d26db3582afd521549eae2b66769
dpiyumantha <dimalkapiyumantha@gmail.com>
2021-05-31 21:38:23 +0530

    refactor: change of variable name

--- reverse-a-number/reverseNumber.js
+++ reverse-a-number/reverseNumber.js
@@ -1,19 +1,20 @@
 function reverseANumber(number) {
   tempNumber = number;
   places = 0;
-  result = 0;//1
+  result = 0;
+
   while (tempNumber >= 1) {
     tempNumber = tempNumber / 10;
     places++;
   }
-  // console.log(places);
+  //Going through digits of a number from front
+  //Multiply by 10's multiples from 1
+  //Take addition as the reversed number
   for (let i = 0; i < places; i++) {
     divider = Math.pow(10, places - 1 - i);
-    // console.log(number,"  ",divider);
-    let a = Math.floor(number / divider);
-    number = number - a * divider;
-    result = result + a * Math.pow(10, i);
-    // console.log(a);
+    let remainder = Math.floor(number / divider);
+    number = number - remainder * divider;
+    result = result + remainder * Math.pow(10, i);
   }
   return result;
 }
