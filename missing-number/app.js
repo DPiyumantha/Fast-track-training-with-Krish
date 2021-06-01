09068a8e44aa4b8dc8250abddad75fa4b0978f75
dpiyumantha <dimalkapiyumantha@gmail.com>
2021-06-01 19:27:55 +0530

    feat: find multiple missing numbers

--- missing-number/app.js
+++ missing-number/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const {printMissingNumber} = require('./missing-number');
+const {printMissingNumber,printMultipleMissingNumbers} = require('./missing-number');
 const port = 3000;
 app.use(express.json());
 
@@ -10,5 +10,29 @@ app.post('/missing-number', (req, res)=>{
     res.send(printMissingNumber(req.body.dataset));
 })
 
+app.post('/multiple-missing-number', (req, res)=>{
+    res.send(printMissingNumber(req.body.dataset));
+})
+
 app.listen(port,()=>{console.log("Listening on port ", port);
 console.log("Sample request body : ",'{"dataset":[1,2,3,4,6]}')})
+
+//Examples
+
+console.log("==================================Example 1\n")
+console.log("Missing multiple numbers")
+console.log("Given sequence ",[2,3,4,7,11])
+console.log(printMultipleMissingNumbers([2,3,4,7,11]))
+console.log("==================================Example 2\n")
+console.log("Missing multiple numbers")
+console.log("Given sequence ",[1,2,3,4,5])
+console.log(printMultipleMissingNumbers([1,2,3,4,5]))
+console.log("==================================Example 3\n")
+console.log("Missing one number")
+console.log("Given sequence ",[1,2,3,5,6,7])
+console.log(printMissingNumber([1,2,3,5,6,7]))
+console.log("==================================Example 4\n")
+console.log("Missing one number")
+console.log("Given sequence ",[2,3,5,6,7,4])
+console.log(printMissingNumber([2,3,5,6,7,4]))
+console.log("===========================================")
--- missing-number/missing-number.js
+++ missing-number/missing-number.js
@@ -14,4 +14,21 @@ function printMissingNumber(arr) {
   return `You're missing ${res}`;
 }
 
-module.exports = {printMissingNumber}
+function printMultipleMissingNumbers(arr) {
+  arr = arr.filter((el) => !isNaN(el)).sort((a, b) => a - b);
+  const missingNumbers = [];
+  multipleMissingNumbers(arr, missingNumbers);
+  if(missingNumbers.length<1)return `You're missing ${arr[0]-1} or ${arr[arr.length-1]+1}`
+  return `You're missing ${missingNumbers}`
+}
+
+function multipleMissingNumbers(arr, result) {
+  
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 1; j < arr[i + 1] - arr[i]; j++) {
+      result.push(arr[i] + j);
+    }
+  }
+}
+
+module.exports = { printMissingNumber, printMultipleMissingNumbers };
